3332175ec60b3ceaf607fb52b5f3c969c5210159
Monty Taylor <mordred@inaugust.com>
2014-02-19 22:41:33 -0800

    Reset the client object before creating the image
    In situations where the connection between nodepool and the cloud
    it manages may be through an evil proxy, the fact that the
    requests library in python-novaclient keeps port connections open
    means that the evil proxy might timeout the connection during the
    period of time that the image is being built.
    
    Catch proxy errors in the task running and make an attempt to reset
    and retry once, to ensure that stale client object issues will not
    kill us.
    
    Change-Id: I3d69de48be1993c04356d209f622b0a17e6e8d03

--- nodepool/tests/test_nodepool.py
+++ nodepool/tests/test_nodepool.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import mock.Mock as Mock
 import os
 import tempfile
 import threading
@@ -151,3 +152,17 @@ class TestNodepool(tests.DBTestCase):
                                      state=nodedb.READY)
             self.assertEqual(len(nodes), 1)
             self.assertEqual(nodes[0].az, 'az1')
+
+    def test_proxy_timeout(self):
+        """Test that we re-run a task after a ProxyError"""
+        configfile = self.setup_config('node.yaml')
+        pool = nodepool.nodepool.NodePool(configfile, watermark_sleep=1)
+        pool.start()
+        provider = pool.config.providers['fake-provider']
+        manager = pool.getProviderManager(provider)
+
+        task = nodepool.Task()
+        ProxyError = novaclient.client.requests.adapters.ProxyError
+        task.run = Mock(side_effect=[ProxyError,True])
+
+        manager.submitTask(task)
