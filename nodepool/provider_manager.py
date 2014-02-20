bd2a96e3a86acbd2f82ba7611a486f9eabee340a
Monty Taylor <mordred@inaugust.com>
2014-02-19 22:41:33 -0800

    Reset the client object before creating the image
    In situations where the connection between nodepool and the cloud
    it manages may be through an evil proxy, the fact that the
    requests library in python-novaclient keeps port connections open
    means that the evil proxy might timeout the connection during the
    period of time that the image is being built.
    
    Catch proxy errors in the task running and make an attempt to reset
    and retry once, to ensure that stale client object issues will not
    kill us.
    
    Change-Id: I3d69de48be1993c04356d209f622b0a17e6e8d03

--- nodepool/provider_manager.py
+++ nodepool/provider_manager.py
@@ -20,6 +20,7 @@ import logging
 import paramiko
 import novaclient
 import novaclient.client
+import requests
 import time
 
 import fakeprovider
@@ -202,7 +203,7 @@ class ProviderManager(TaskManager):
         super(ProviderManager, self).__init__(None, provider.name,
                                               provider.rate)
         self.provider = provider
-        self._client = self._getClient()
+        self.resetClient()
         self._images = {}
         self._cloud_metadata_read = False
         self.__flavors = {}
@@ -240,6 +241,16 @@ class ProviderManager(TaskManager):
         if self.provider.auth_url == 'fake':
             return fakeprovider.FAKE_CLIENT
         return novaclient.client.Client(*args, **kwargs)
+
+    def runTask(self, task):
+        try:
+            task.run(self._client)
+        except requests.adapters.ProxyError:
+            self.resetClient()
+            task.run(self._client)
+
+    def resetClient(self):
+        self._client = self._getClient()
 
     def _getFlavors(self):
         l = [dict(id=f.id, ram=f.ram, name=f.name)
@@ -376,6 +387,7 @@ class ProviderManager(TaskManager):
                 return newip['ip']
 
     def createImage(self, server_id, image_name):
+        self.reset_client()
         return self.submitTask(CreateImageTask(server=server_id,
                                                image_name=image_name))
 
