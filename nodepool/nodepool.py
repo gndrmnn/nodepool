65ba4861efa4b737655911025f84806529dd0872
Clark Boylan <clark.boylan@gmail.com>
2014-10-24 12:57:27 -0700

    Test label with snapshot and dib images
    We want to test that we can build labels provided by both snapshot and
    dib built images. Set up a test case to cover this particular scenario.
    
    Change-Id: Ie640d82f9a0ae58dd04f1ea005c024752f695fd5

--- nodepool/nodepool.py
+++ nodepool/nodepool.py
@@ -1195,23 +1195,6 @@ class NodePool(threading.Thread):
                 d.release = diskimage.get('release', '')
                 d.qemu_img_options = diskimage.get('qemu-img-options', '')
 
-        for label in config['labels']:
-            l = Label()
-            l.name = label['name']
-            newconfig.labels[l.name] = l
-            l.image = label['image']
-            l.min_ready = label.get('min-ready', 2)
-            l.subnodes = label.get('subnodes', 0)
-            l.ready_script = label.get('ready-script')
-            l.providers = {}
-            for provider in label['providers']:
-                p = LabelProvider()
-                p.name = provider['name']
-                l.providers[p.name] = p
-
-            # if image is in diskimages, mark it to build once
-            l.is_diskimage = (l.image in newconfig.diskimages)
-
         for provider in config['providers']:
             p = Provider()
             p.name = provider['name']
@@ -1268,6 +1251,26 @@ class NodePool(threading.Thread):
                         self.log.error("Invalid metadata for %s; ignored"
                                        % i.name)
                         i.meta = {}
+
+        for label in config['labels']:
+            l = Label()
+            l.name = label['name']
+            newconfig.labels[l.name] = l
+            l.image = label['image']
+            l.min_ready = label.get('min-ready', 2)
+            l.subnodes = label.get('subnodes', 0)
+            l.ready_script = label.get('ready-script')
+            l.providers = {}
+            l.has_diskimage = []
+            l.has_snapshot = []
+            for provider in label['providers']:
+                p = LabelProvider()
+                p.name = provider['name']
+                l.providers[p.name] = p
+                if newconfig.providers[p.name].images[l.image].diskimage:
+                    l.has_diskimage.append(p.name)
+                else:
+                    l.has_snapshot.append(p.name)
 
         for target in config['targets']:
             t = Target()
@@ -1709,7 +1712,7 @@ class NodePool(threading.Thread):
                 continue
 
             # check if image is there, if not, build it
-            if label.is_diskimage:
+            if label.has_diskimage:
                 found = False
                 for dib_image in session.getDibImages():
                     if (dib_image.image_name == label.image and
@@ -1753,7 +1756,7 @@ class NodePool(threading.Thread):
                             if available_images:
                                 self.uploadImage(session, provider.name,
                                                  label.image)
-            else:
+            elif label.has_snapshot:
                 # snapshots
                 for provider in label.providers.values():
                     found = False
@@ -1785,7 +1788,7 @@ class NodePool(threading.Thread):
                 continue
 
             # check if image is there, if not, build it
-            if label.is_diskimage:
+            if label.has_diskimage:
                 self.buildImage(self.config.diskimages[label.image])
                 needs_build = True
         if needs_build:
@@ -1797,11 +1800,12 @@ class NodePool(threading.Thread):
                 # Label is configured to be disabled, skip creating the image.
                 continue
 
-            for provider in label.providers.values():
-                if label.is_diskimage:
-                    self.uploadImage(session, provider.name, label.image)
-                else:
-                    self.updateImage(session, provider.name, label.image)
+            # Handle cases where labels might have images built by dib
+            # and images that are snapshotted.
+            for provider in label.has_diskimage:
+                self.uploadImage(session, provider, label.image)
+            for provider in label.has_snapshot:
+                self.updateImage(session, provider, label.image)
 
     def updateImage(self, session, provider_name, image_name):
         try:
@@ -1811,15 +1815,14 @@ class NodePool(threading.Thread):
                 "Could not update image %s on %s", image_name, provider_name)
 
     def _updateImage(self, session, provider_name, image_name):
-        # check type of image depending on label
-        is_diskimage = (image_name in self.config.diskimages)
-        if is_diskimage:
+        provider = self.config.providers[provider_name]
+        image = provider.images[image_name]
+        # check type of image depending on diskimage flag
+        if image.diskimage:
             raise Exception(
                 "Cannot update disk image images. "
                 "Please build and upload images")
 
-        provider = self.config.providers[provider_name]
-        image = provider.images[image_name]
         if not image.setup:
             raise Exception(
                 "Invalid image config. Must specify either "
@@ -1836,13 +1839,6 @@ class NodePool(threading.Thread):
         return t
 
     def buildImage(self, image):
-        # check type of image depending on label
-        is_diskimage = (image.name in self.config.diskimages)
-        if not is_diskimage:
-            raise Exception(
-                "Cannot build non disk image images. "
-                "Please create snapshots for them")
-
         # check if we already have this item in the queue
         with self.getDB().getSession() as session:
             queued_images = session.getBuildingDibImagesByName(image.name)
