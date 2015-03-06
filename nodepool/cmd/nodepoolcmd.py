9f988fa8bb5d1b13863a028cc43276e3f2ef9da0
Brian Hunter <bhunter@tesora.com>
2015-03-05 21:16:25 -0500

    Pretty Age in nodepool list
    This outputs the age column a format like "1m 03s"
    I find this more readable than hundredths of hours.
    I'm unaware of programatic uses of this output.
    
    The code was cloned from how Zuul generates status messages for gerrit.
    
    Change-Id: I9ab1bc1cadedbd64522cbe72d6004f5e95c98db1

--- nodepool/cmd/nodepoolcmd.py
+++ nodepool/cmd/nodepoolcmd.py
@@ -135,19 +135,28 @@ class NodePoolCmd(object):
 
     def list(self, node_id=None):
         t = PrettyTable(["ID", "Provider", "AZ", "Label", "Target", "Hostname",
-                         "NodeName", "Server ID", "IP", "State",
-                         "Age (hours)"])
+                         "NodeName", "Server ID", "IP", "State", "Age"])
         t.align = 'l'
         now = time.time()
         with self.pool.getDB().getSession() as session:
             for node in session.getNodes():
                 if node_id and node.id != node_id:
                     continue
+
+                dt = int(now - node.state_time)
+                m, s = divmod(dt, 60)
+                h, m = divmod(m, 60)
+                if h:
+                    age = '%dh %02dm %02ds' % (h, m, s)
+                elif m:
+                    age = '%dm %02ds' % (m, s)
+                else:
+                    age = '%ds' % (s)
+
                 t.add_row([node.id, node.provider_name, node.az,
                            node.label_name, node.target_name, node.hostname,
                            node.nodename, node.external_id, node.ip,
-                           nodedb.STATE_NAMES[node.state],
-                           '%.02f' % ((now - node.state_time) / 3600)])
+                           nodedb.STATE_NAMES[node.state], age])
             print t
 
     def dib_image_list(self):
