0e61c49d8010f739e1890acc367bff8793014089
Clark Boylan <clark.boylan@gmail.com>
2015-03-06 09:09:02 -0800

    Test that dib failures don't stop node builds
    Test that when a dib image fails to build nodepool continues to build
    nodes using images it does have available.
    
    To check that the dib build actually fails update the diskimage-create
    fake script to touch a file that can be checked in the TMPDIR if the
    TMPDIR isn't set properly.
    
    Change-Id: Ie7c13af319141ec3cf8d43155358157e7d9274b8

--- nodepool/tests/test_nodepool.py
+++ nodepool/tests/test_nodepool.py
@@ -18,18 +18,20 @@ import tempfile
 import threading
 import time
 
+import fixtures
+
 from nodepool import tests
 from nodepool import nodedb
 import nodepool.nodepool
 
 
 class TestNodepool(tests.DBTestCase):
-    def setup_config(self, filename):
+    def setup_config(self, filename, **kwargs):
         configfile = os.path.join(os.path.dirname(tests.__file__),
                                   'fixtures', filename)
         config = open(configfile).read()
         (fd, path) = tempfile.mkstemp()
-        os.write(fd, config.format(dburi=self.dburi))
+        os.write(fd, config.format(**kwargs))
         os.close(fd)
         return path
 
@@ -94,7 +96,7 @@ class TestNodepool(tests.DBTestCase):
 
     def test_node(self):
         """Test that an image and node are created"""
-        configfile = self.setup_config('node.yaml')
+        configfile = self.setup_config('node.yaml', dburi=self.dburi)
         pool = nodepool.nodepool.NodePool(configfile, watermark_sleep=1)
         pool.start()
         self.addCleanup(pool.stop)
@@ -110,7 +112,7 @@ class TestNodepool(tests.DBTestCase):
 
     def test_dib_node(self):
         """Test that a dib image and node are created"""
-        configfile = self.setup_config('node_dib.yaml')
+        configfile = self.setup_config('node_dib.yaml', dburi=self.dburi)
         pool = nodepool.nodepool.NodePool(configfile, watermark_sleep=1)
         pool.start()
         self.addCleanup(pool.stop)
@@ -126,7 +128,8 @@ class TestNodepool(tests.DBTestCase):
 
     def test_dib_and_snap_label(self):
         """Test that a label with dib and snapshot images build."""
-        configfile = self.setup_config('node_dib_and_snap.yaml')
+        configfile = self.setup_config('node_dib_and_snap.yaml',
+                                       dburi=self.dburi)
         pool = nodepool.nodepool.NodePool(configfile, watermark_sleep=1)
         pool.start()
         self.addCleanup(pool.stop)
@@ -146,9 +149,38 @@ class TestNodepool(tests.DBTestCase):
                                      state=nodedb.READY)
             self.assertEqual(len(nodes), 1)
 
+    def test_dib_and_snap_fail(self):
+        """Test that snap based nodes build when dib fails."""
+        tmpdir = self.useFixture(fixtures.TempDir())
+        configfile = self.setup_config('node_dib_and_snap_fail.yaml',
+                                       dburi=self.dburi,
+                                       tmpdir=tmpdir.path)
+        pool = nodepool.nodepool.NodePool(configfile, watermark_sleep=1)
+        # Set env vars to make fake-diskimage-create write failure to tmpdir
+        pool.start()
+        self.addCleanup(pool.stop)
+        # fake-provider1 will fail to build fake-dib-image
+        self.waitForImage(pool, 'fake-provider2', 'fake-dib-image')
+        self.waitForNodes(pool)
+        self.assertTrue(os.path.exists(os.path.join(tmpdir.path, 'fail')))
+
+        with pool.getDB().getSession() as session:
+            # fake-provider1 uses dib.
+            nodes = session.getNodes(provider_name='fake-provider1',
+                                     label_name='fake-label',
+                                     target_name='fake-target',
+                                     state=nodedb.READY)
+            self.assertEqual(len(nodes), 0)
+            # fake-provider2 uses snapshots.
+            nodes = session.getNodes(provider_name='fake-provider2',
+                                     label_name='fake-label',
+                                     target_name='fake-target',
+                                     state=nodedb.READY)
+            self.assertEqual(len(nodes), 2)
+
     def test_subnodes(self):
         """Test that an image and node are created"""
-        configfile = self.setup_config('subnodes.yaml')
+        configfile = self.setup_config('subnodes.yaml', dburi=self.dburi)
         pool = nodepool.nodepool.NodePool(configfile, watermark_sleep=1)
         pool.start()
         self.addCleanup(pool.stop)
@@ -173,7 +205,7 @@ class TestNodepool(tests.DBTestCase):
 
     def test_node_az(self):
         """Test that an image and node are created with az specified"""
-        configfile = self.setup_config('node_az.yaml')
+        configfile = self.setup_config('node_az.yaml', dburi=self.dburi)
         pool = nodepool.nodepool.NodePool(configfile, watermark_sleep=1)
         pool.start()
         self.addCleanup(pool.stop)
