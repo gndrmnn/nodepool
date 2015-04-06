501e7bc38c7324caf73be7eba990ca05f7373e59
Yolanda Robla <yolanda.robla-mota@hp.com>
2015-04-06 10:49:19 +0200

    Different queues for slow tasks.
    Use a two queues approach in the manager. Move create
    and delete server requests to secondary queue, and all
    the other requests to main one, to prevent locking
    queue in slow requests.
    
    Change-Id: I3ed4fe90ee128883af2dba88621b28c9f88f6105

--- nodepool/tests/test_nodepool.py
+++ nodepool/tests/test_nodepool.py
@@ -299,7 +299,7 @@ class TestNodepool(tests.DBTestCase):
         # always raises a ProxyError. If our client reset works correctly
         # then we will create a new client object, which in this case would
         # be a new fake client in place of the bad client.
-        manager._client = nodepool.fakeprovider.get_bad_client()
+        manager.setClient(nodepool.fakeprovider.get_bad_client())
 
         # The only implemented function for the fake and bad clients
         # If we don't raise an uncaught exception, we pass
@@ -307,8 +307,23 @@ class TestNodepool(tests.DBTestCase):
 
         # Now let's do it again, but let's prevent the client object from being
         # replaced and then assert that we raised the exception that we expect.
-        manager._client = nodepool.fakeprovider.BAD_CLIENT
+        manager.setClient(nodepool.fakeprovider.BAD_CLIENT)
         manager._getClient = lambda: nodepool.fakeprovider.get_bad_client()
 
         with ExpectedException(requests.exceptions.ProxyError):
             manager.listExtensions()
+
+    def test_managers_stopped(self):
+        configfile = self.setup_config('node.yaml')
+        pool = self.useNodepool(configfile, watermark_sleep=1)
+        pool.start()
+        pool.stop()
+        self.waitForNodes(pool)
+
+        # ensure that no nodes have been created
+        with pool.getDB().getSession() as session:
+            nodes = session.getNodes(provider_name='fake-provider',
+                                     label_name='fake-label',
+                                     target_name='fake-target',
+                                     state=nodedb.READY)
+            self.assertEqual(len(nodes), 0)
