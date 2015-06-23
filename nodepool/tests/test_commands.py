116909dda7f192c91a4125e5af19a2e6ef73a392
Gregory Haynes <greg@greghaynes.net>
2015-06-23 23:57:02 +0000

    Trigger image builds using gearman
    Move image building into its own module and use gearman to perform the
    triggering of image builds. This is the first patch in a process to move
    image building into a completely separate worker process.
    
    Note that this changes the semantics of nodepool commands - they now do
    not execute builds in the context of a new nodepool process but
    communicate over gearman to an already running gearman worker.
    
    Change-Id: Id0a64a8a957a5130f319ccd5beb3f45ea419eea6

--- nodepool/tests/test_commands.py
+++ nodepool/tests/test_commands.py
@@ -20,10 +20,21 @@ import fixtures
 import mock
 
 from nodepool.cmd import nodepoolcmd
-from nodepool import tests
+from nodepool import nodepool, tests
 
 
 class TestNodepoolCMD(tests.DBTestCase):
+    def _useBuilder(self, configfile):
+        # XXX:greghaynes This is a gross hack for while the builder depends on
+        # nodepool because we have a circular dep. Remove this when builder
+        # gets its own config.
+        pool = nodepool.NodePool(configfile, run_builder=False)
+        config = pool.loadConfig()
+        pool.reconfigureDatabase(config)
+        pool.setConfig(config)
+        self.builder = tests.BuilderFixture(pool)
+        self.useFixture(self.builder)
+
     def patch_argv(self, *args):
         argv = ["nodepool", "-s", self.secure_conf]
         argv.extend(args)
@@ -67,6 +78,7 @@ class TestNodepoolCMD(tests.DBTestCase):
 
     def test_dib_image_update(self):
         configfile = self.setup_config("node_dib.yaml")
+        self._useBuilder(configfile)
         self.patch_argv("-c", configfile, "image-update",
                         "fake-dib-provider", "fake-dib-image")
         nodepoolcmd.main()
@@ -75,6 +87,7 @@ class TestNodepoolCMD(tests.DBTestCase):
 
     def test_dib_snapshot_image_update(self):
         configfile = self.setup_config("node_dib_and_snap.yaml")
+        self._useBuilder(configfile)
         self.patch_argv("-c", configfile, "image-update",
                         "fake-provider1", "fake-dib-image")
         nodepoolcmd.main()
@@ -86,6 +99,7 @@ class TestNodepoolCMD(tests.DBTestCase):
 
     def test_dib_snapshot_image_update_all(self):
         configfile = self.setup_config("node_dib_and_snap.yaml")
+        self._useBuilder(configfile)
         self.patch_argv("-c", configfile, "image-update",
                         "all", "fake-dib-image")
         nodepoolcmd.main()
@@ -94,6 +108,7 @@ class TestNodepoolCMD(tests.DBTestCase):
 
     def test_image_update_all(self):
         configfile = self.setup_config("node_cmd.yaml")
+        self._useBuilder(configfile)
         self.patch_argv("-c", configfile, "image-update",
                         "all", "fake-image1")
         nodepoolcmd.main()
