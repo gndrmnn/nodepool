346bc34112c974d7c34cf184e3ab988d6e6e97ae
Ramy Asselin <ramy.asselin@hp.com>
2015-09-08 12:58:06 -0700

    Run nodepool commands as nodepool user.
    Some nodepool files access is only availabe to the nodepol user.
    Ensure tests are run as the nodepool user to access private
    nodepool files.
    
    Change-Id: I4fd5784d79ccbf6d16fffc04a80d862d83262fdb

--- tools/check_devstack_plugin.sh
+++ tools/check_devstack_plugin.sh
@@ -3,11 +3,12 @@
 # Sleep long enough for the below checks to have a chance
 # at being completed.
 sleep 15m
+
 # Check that snapshot image built
-nodepool image-list | grep ready | grep trusty-server
+sudo -u nodepool nodepool image-list | grep ready | grep trusty-server
 # check that dib image built
-nodepool image-list | grep ready | grep ubuntu-dib
+sudo -u nodepool nodepool image-list | grep ready | grep ubuntu-dib
 # check snapshot image was bootable
-nodepool list | grep ready | grep trusty-server
+sudo -u nodepool nodepool list | grep ready | grep trusty-server
 # check dib image was bootable
-nodepool list | grep ready | grep ubuntu-dib
+sudo -u nodepool nodepool list | grep ready | grep ubuntu-dib
