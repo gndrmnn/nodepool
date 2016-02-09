43782d650c7169aed8560d072181f535e869af5f
Clark Boylan <clark.boylan@gmail.com>
2016-02-09 15:35:41 -0800

    Show innodb status before dropping db
    We occasionally have tests fail on timeout errors when dropping the per
    test db does not happen quickly enough. To aid in debugging this show
    the innodb status before dropping the database to get transaction and
    other info.
    
    Change-Id: Ia0b08598887b1779d1d0d8285b0fc9393eebfbd1

--- nodepool/tests/__init__.py
+++ nodepool/tests/__init__.py
@@ -279,6 +279,8 @@ class MySQLSchemaFixture(fixtures.Fixture):
                              passwd="openstack_citest",
                              db="openstack_citest")
         cur = db.cursor()
+        # Dump transaction state to debug failed db drops.
+        cur.execute("show engine innodb status" % self.name)
         cur.execute("drop database %s" % self.name)
 
 
