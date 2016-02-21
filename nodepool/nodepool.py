a1c8df6a3145108078b737843c59ce42394b9b40
Monty Taylor <mordred@inaugust.com>
2016-02-21 13:29:01 -0600

    Add the ability to use ansible for ready scripts
    Baking scripts into the base image is a design choice intended to avoid
    copying the same code to each host tens of thousands of times per day.
    The trade off with that choice is the need to spin new base images any
    time a change needs to be landed to ready scripts. As zuul v3 is already
    looking towards a future where we'll be using ansible to execute remote
    code at run time, it's possible that it's time to allow for the choice
    of greater runtime flexibility and responsiveness at the expense of
    additional copying.
    
    As the scripts will still be baked in to the image, a hybrid world where
    the ready playbook runs and executes some more static and unchanging
    bits that are in the scripts dir may wind up being the most
    advantageous.
    
    In any case, this adds the ability to have a ready playbook. It depends
    on a not-yet-released library which is itself extracted from code in the
    zuul v3 branch of zuul.
    
    Change-Id: I5c0f2dfdf6c9d211dd80f1d819aac7ad87064d2a

--- nodepool/nodepool.py
+++ nodepool/nodepool.py
@@ -26,6 +26,7 @@ import paramiko
 import pprint
 import random
 import re
+import runansible
 import threading
 import time
 from uuid import uuid4
@@ -56,6 +57,15 @@ IMAGE_CLEANUP = 8 * HOURS    # When to start deleting an image that is not
                              # READY or is not the current or previous image
 DELETE_DELAY = 1 * MINS      # Delay before deleting a node that has completed
                              # its job.
+
+
+def _ansible_nodelist(nodelist):
+    """Takes a list of tuples of the form (role, host) and returns
+       a list of tuples of the form (host, [role], [])"""
+    new_list = []
+    for role, host in nodelist:
+        new_list.append((host, [role], []))
+    return new_list
 
 
 class LaunchNodepoolException(Exception):
@@ -533,10 +543,12 @@ class NodeLauncher(threading.Thread):
 
         nodelist = []
         for subnode in self.node.subnodes:
-            nodelist.append(('sub', subnode))
+            nodelist.append(('subnodes', subnode))
         nodelist.append(('primary', self.node))
 
         self.writeNodepoolInfo(nodelist)
+        if self.label.playbook:
+            self.runPlaybook(nodelist)
         if self.label.ready_script:
             self.runReadyScript(nodelist)
 
@@ -701,6 +713,12 @@ class NodeLauncher(threading.Thread):
                      "cd /opt/nodepool-scripts && %s ./%s %s" %
                      (env_vars, self.label.ready_script, n.hostname),
                      output=True)
+
+    def runPlaybook(self, nodelist):
+        with runansible.AnsibleRunner() as runner:
+            runner.prepare_files(_ansible_nodelist(nodelist))
+            runner.copy_playbook(self.label.playbook)
+            runner.run()
 
 
 class SubNodeLauncher(threading.Thread):
