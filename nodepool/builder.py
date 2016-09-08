37783abfe74bb1f6597290b621002726319b84b0
David Shrewsbury <shrewsbury.dave@gmail.com>
2016-09-08 10:37:07 -0400

    Add skeleton for DIB build cleanup
    Adds a periodic check for deleting local DIB builds marked as needing
    deletion, as well as for removing build data in ZooKeeper that is
    irrelevant (unused or junk nodes).
    
    Change-Id: I01ea82937664ae4cb0b80b7efe85a19ad18d7f69

--- nodepool/builder.py
+++ nodepool/builder.py
@@ -230,6 +230,31 @@ class BuildWorker(BaseWorker):
         build_data['filename'] = ''
         return build_data
 
+    def _deleteLocalBuild(self):
+        # TODO(Shrews): implement this
+        pass
+
+    def _checkForCleanup(self):
+        '''
+        Perform any local or ZooKeeper cleanup tasks.
+
+        If a build is marked as 'delete' and it was built on this host, then
+        we clean up the local build and delete the znode. We don't touch
+        those that were NOT built on this host.
+        '''
+        for image in self._config.diskimages.keys():
+            chosen = self._zk.getBuildsWithStates(image,
+                                                  ['delete', 'unused', ''])
+
+            for build_num, data in chosen.items():
+                state = data.get('state', '')
+                builder = data.get('builder', '')
+                if state == 'delete' and builder == self._hostname:
+                    self._deleteLocalBuild()
+                    self._zk.deleteBuild(image, build_num)
+                elif state in ('unused', ''):
+                    self._zk.deleteBuild(image, build_num)
+
     def run(self):
         self._running = True
         while self._running:
@@ -238,6 +263,7 @@ class BuildWorker(BaseWorker):
             self._checkForZooKeeperChanges(new_config)
             self._config = new_config
 
+            self._checkForCleanup()
             self._checkForScheduledImageUpdates()
             self._checkForManualBuildRequest()
 
