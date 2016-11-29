204804f42a6576d05c90a716b2af08a13b11c91c
David Shrewsbury <shrewsbury.dave@gmail.com>
2016-11-29 11:33:02 -0500

    DNM: Debug of uploading
    Change-Id: I334f1ecba77d6cd284d425ccbfe68dbe230c834f

--- nodepool/builder.py
+++ nodepool/builder.py
@@ -836,7 +836,13 @@ class UploadWorker(BaseWorker):
                 upload = self._zk.getMostRecentBuildImageUploads(
                     1, image.name, build.id, provider.name, zk.READY)
                 if upload:
+                    self.log.debug("Will NOT upload %s:%s to %s, found %s" %
+                                   (image.name, build.id, upload))
                     return
+
+                self.log.debug(
+                    "Will upload %s:%s to %s" %
+                    (image.name, build.id, provider.name))
 
                 # New upload number with initial state 'uploading'
                 data = zk.ImageUpload()
@@ -850,6 +856,8 @@ class UploadWorker(BaseWorker):
                 # Set final state
                 self._zk.storeImageUpload(image.name, build.id,
                                           provider.name, data, upnum)
+
+                self.log.debug("Upload complete: %s" % data)
         except exceptions.ZKLockException:
             # Lock is already held. Skip it.
             pass
