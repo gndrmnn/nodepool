4d713c6727f91e358681ea7d3bd898695cb1b37b
Ian Wienand <iwienand@redhat.com>
2016-11-30 08:35:45 +1100

    [DNM] test
    test
    
    Change-Id: I158910342158882b5ab5b9a964894a07c6a95e7b

--- nodepool/nodepool.py
+++ nodepool/nodepool.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Copyright (C) 2011-2014 OpenStack Foundation
-#
+# XXX
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
