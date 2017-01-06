889821e156000b32279d784b8e1849157d146518
David Shrewsbury <shrewsbury.dave@gmail.com>
2017-01-06 13:48:42 -0500

    Set valid states per data model class
    Some states are not valid within a certain context. For example,
    an ImageBuild cannot be in the UPLOADING state, nor can an ImageUpload
    be in the BUILDING state. Set the list of valid states in each data
    model class.
    
    Change-Id: I5efc3809042d6dc850fe07f0ae0362d1b9870d4c

--- nodepool/tests/test_zk.py
+++ nodepool/tests/test_zk.py
@@ -543,18 +543,18 @@ class TestZKModel(tests.BaseTestCase):
 
     def test_BaseModel_toDict(self):
         o = zk.BaseModel('0001')
-        o.state = zk.BUILDING
         d = o.toDict()
         self.assertNotIn('id', d)
-        self.assertEqual(o.state, d['state'])
-        self.assertIsNotNone(d['state_time'])
 
     def test_ImageBuild_toDict(self):
         o = zk.ImageBuild('0001')
+        o.state = zk.BUILDING
         o.builder = 'localhost'
         o.formats = ['qemu', 'raw']
 
         d = o.toDict()
+        self.assertEqual(o.state, d['state'])
+        self.assertIsNotNone(d['state_time'])
         self.assertNotIn('id', d)
         self.assertEqual(','.join(o.formats), d['formats'])
         self.assertEqual(o.builder, d['builder'])
@@ -578,6 +578,7 @@ class TestZKModel(tests.BaseTestCase):
 
     def test_ImageUpload_toDict(self):
         o = zk.ImageUpload('0001', '0003')
+        o.state = zk.UPLOADING
         o.external_id = 'DEADBEEF'
         o.external_name = 'trusty'
 
@@ -621,7 +622,7 @@ class TestZKModel(tests.BaseTestCase):
         now = int(time.time())
         req_id = "500-123"
         d = {
-            'state': zk.READY,
+            'state': zk.REQUESTED,
             'state_time': now
         }
 
