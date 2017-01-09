9b630db3fa97e411954cad8b69e25b4c9f882140
David Shrewsbury <shrewsbury.dave@gmail.com>
2017-01-09 13:09:10 -0500

    Remove --no-launches nodepoold option
    This isn't needed in v3.
    
    Change-Id: I4ebe2383674d149958cf7c55791ebb0da9e22849

--- nodepool/cmd/nodepoold.py
+++ nodepool/cmd/nodepoold.py
@@ -81,7 +81,6 @@ class NodePoolDaemon(nodepool.cmd.NodepoolApp):
                             default=4, help='number of upload workers',
                             type=int)
         parser.add_argument('--no-deletes', action='store_true')
-        parser.add_argument('--no-launches', action='store_true')
         parser.add_argument('--no-webapp', action='store_true')
         parser.add_argument('--version', dest='version', action='store_true',
                             help='show version')
@@ -102,8 +101,7 @@ class NodePoolDaemon(nodepool.cmd.NodepoolApp):
         self.setup_logging()
         self.pool = nodepool.nodepool.NodePool(self.args.secure,
                                                self.args.config,
-                                               self.args.no_deletes,
-                                               self.args.no_launches)
+                                               self.args.no_deletes)
         if self.args.builder:
             self.builder = nodepool.builder.NodePoolBuilder(
                 self.args.config, self.args.build_workers,
