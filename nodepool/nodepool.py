25fe19a36f699d077b59cfde723c04e5d99d45ae
David Shrewsbury <shrewsbury.dave@gmail.com>
2017-02-16 14:43:40 -0500

    Honor NODE_CLEANUP time again
    Adds back this piece of functionality: For nodes that are not in
    DELETING state, but have been in an otherwise non-READY state for
    NODE_CLEANUP time, and are unlocked, delete them.
    
    Change-Id: I33e5a0d0a19c2454422f2b17bf62094a2098764b

--- nodepool/nodepool.py
+++ nodepool/nodepool.py
@@ -1190,6 +1190,15 @@ class NodeCleanupWorker(threading.Thread):
                 except exceptions.ZKLockException:
                     continue
 
+                # For nodes not in DELETING state, make sure they've been
+                # around for a minimum time before deleting them.
+                if node.state != zk.DELETING:
+                    now = time.time()
+                    time_in_state = now - node.state_time
+                    if time_in_state > NODE_CLEANUP:
+                        zk_conn.unlockNode(node)
+                        continue
+
                 # The InstanceDeleter thread will unlock and remove the
                 # node from ZooKeeper if it succeeds.
                 self._deleteInstance(node)
