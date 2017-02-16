db35205efda4b5585c85ec94e20c509dbe97135d
David Shrewsbury <shrewsbury.dave@gmail.com>
2017-02-16 16:36:08 -0500

    Fix possible race with node request handling
    We were properly double checking the state after locking the request,
    but we weren't actually querying ZK to get the updated request info.
    
    Change-Id: I5764bd53478b94fdb4dd99fb04731f97cc203f73

--- nodepool/nodepool.py
+++ nodepool/nodepool.py
@@ -1076,8 +1076,9 @@ class ProviderWorker(threading.Thread):
             except exceptions.ZKLockException:
                 continue
 
-            # Make sure the state didn't change on us
-            if req.state != zk.REQUESTED:
+            # Make sure the state didn't change on us after getting the lock
+            req = self.zk.getNodeRequest(req_id)
+            if req and req.state != zk.REQUESTED:
                 self.zk.unlockNodeRequest(req)
                 continue
 
