0256f462ca5317f912106d3bea27be3502f2f90b
Paul Belanger <pabelanger@redhat.com>
2017-04-01 13:23:56 -0400

    WIP Fetch server console log if ssh connection fails
    Currently, if the ssh connection fails, we are blind to what the
    possible failures are.  As a result, attempt to fetch the server
    console log to help debug the failure.
    
    Change-Id: I75ccb6d01956fb6052473f44cce8f097a56dd16a
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- nodepool/provider_manager.py
+++ nodepool/provider_manager.py
@@ -218,6 +218,13 @@ class ProviderManager(object):
         with shade_inner_exceptions():
             return self._client.get_server(server_id)
 
+    def getServerConsole(self, server_id):
+        try:
+            with shade_inner_exceptions():
+                return self._client.get_server_console(server_id)
+        except shade.OpenStackCloudException:
+            return None
+
     def waitForServer(self, server, timeout=3600):
         with shade_inner_exceptions():
             return self._client.wait_for_server(
