38ef17811c0d8099071d33fe8eeaba2bf82d73f9
Tristan Cacqueray <tdecacqu@redhat.com>
2017-05-29 00:27:29 +0000

    Collect request handling implementation in an OpenStack driver
    This change moves OpenStack related code to a driver.
    
    Change-Id: I319ce8780aa7e81b079c3f31d546b89eca6cf5f4
    Story: 2001044
    Task: 4614

--- nodepool/driver/openstack/provider.py
+++ nodepool/driver/openstack/provider.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 # Copyright (C) 2011-2013 OpenStack Foundation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,8 +21,8 @@ import operator
 import shade
 
 from nodepool import exceptions
-from nodepool import fakeprovider
 from nodepool.driver import ProviderManager
+from nodepool.launcher import NotFound
 from nodepool.nodeutils import iterate_timeout
 from nodepool.task_manager import ManagerStoppedException
 from nodepool.task_manager import TaskManager
@@ -42,12 +40,9 @@ def shade_inner_exceptions():
         raise
 
 
-class NotFound(Exception):
-    pass
-
-
 class OpenStackProviderManager(ProviderManager):
-    log = logging.getLogger("nodepool.OpenStackProviderManager")
+    log = logging.getLogger("nodepool.driver.openstack.provider."
+                            "OpenStackProviderManager")
 
     def __init__(self, provider, use_taskmanager):
         self.provider = provider
@@ -336,19 +331,3 @@ class OpenStackProviderManager(ProviderManager):
                 # ability to turn off random portions of the OpenStack API.
                 self.__azs = [None]
         return self.__azs
-
-
-class FakeProviderManager(OpenStackProviderManager):
-    def __init__(self, provider, use_taskmanager):
-        self.createServer_fails = 0
-        self.__client = fakeprovider.FakeOpenStackCloud()
-        super(FakeProviderManager, self).__init__(provider, use_taskmanager)
-
-    def _getClient(self):
-        return self.__client
-
-    def createServer(self, *args, **kwargs):
-        while self.createServer_fails:
-            self.createServer_fails -= 1
-            raise Exception("Expected createServer exception")
-        return super(FakeProviderManager, self).createServer(*args, **kwargs)
