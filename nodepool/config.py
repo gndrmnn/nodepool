3fa82838120dee2893888d3b6dd88114f4c3ec97
Tristan Cacqueray <tdecacqu@redhat.com>
2017-06-17 12:44:16 +0000

    Implement a static driver for Nodepool
    This change also modifies the generic ProviderManager to handle
    manager without launch_manager. It also adapts the Nodepool class
    to handle provider without Image management.
    
    Change-Id: I065f2b42af49a57218f1c04a08b3ddd15ccc0832
    Story: 2001044
    Task: 4615

--- nodepool/config.py
+++ nodepool/config.py
@@ -268,10 +268,32 @@ def loadConfig(config_path):
             pp.name = pool['name']
             pp.provider = p
             p.pools[pp.name] = pp
-            pp.max_servers = pool['max-servers']
-            pp.azs = pool.get('availability-zones')
+            pp.max_servers = pool.get('max-servers', None)
+            pp.azs = pool.get('availability-zones', [])
             pp.networks = pool.get('networks', [])
             pp.labels = {}
+            pp.nodes = []
+            for node in pool.get('nodes', []):
+                pp.nodes.append({
+                    'name': node['name'],
+                    'labels': node['labels'].split(),
+                    'host-key': node['host-key'],
+                    'timeout': int(node.get('timeout', 5)),
+                    'ssh_port': int(node.get('ssh_port', 22)),
+                    'username': node.get('username', 'root'),
+                    'max_concurrency': int(node.get('max_concurrency', 1)),
+                })
+                for label in node['labels'].split():
+                    pl = ProviderLabel()
+                    pl.diskimage = None
+                    pl.cloud_image = None
+                    pl.min_ram = None
+                    pl.flavor_name = None
+                    pl.key_name = None
+                    pl.name = label
+                    pl.pool = pp
+                    pp.labels[pl.name] = pl
+                    newconfig.labels[label].pools.append(pp)
             for label in pool.get('labels', []):
                 pl = ProviderLabel()
                 pl.name = label['name']
