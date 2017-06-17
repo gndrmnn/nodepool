fe0de59e4a37086d8d1ad29b7d9407bb52c7ebfa
Tristan Cacqueray <tdecacqu@redhat.com>
2017-06-17 12:44:16 +0000

    Implement a static driver for Nodepool
    This change also modifies the generic ProviderManager to handle
    manager without launch_manager. It also adapts the Nodepool class
    to handle provider without Image management.
    
    Depends-On: I5e6d3969ae04f90abd1a3fd908c160cda4791bad
    Change-Id: I065f2b42af49a57218f1c04a08b3ddd15ccc0832
    Story: 2001044
    Task: 4615

--- nodepool/cmd/config_validator.py
+++ nodepool/cmd/config_validator.py
@@ -49,11 +49,24 @@ class ConfigValidator:
                            v.Any(label_min_ram, label_flavor_name),
                            v.Any(label_diskimage, label_cloud_image))
 
+        pool_node_main = {
+            v.Required('name'): str,
+            v.Required('labels'): str,
+            v.Required('host-key'): str,
+            'username': str,
+            'timeout': int,
+            'ssh-port': int,
+            'max-concurrency': int,
+        }
+
+        pool_node = v.All(pool_node_main)
+
         pool = {
             'name': str,
             'networks': [str],
             'max-servers': int,
             'labels': [pool_label],
+            'nodes': [pool_node],
             'availability-zones': [str],
             }
 
