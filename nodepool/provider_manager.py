ed346a81bdf2d78e81e479df17bdeb48889d4951
Tristan Cacqueray <tdecacqu@redhat.com>
2017-06-17 12:47:06 +0000

    Implement an OpenContainer driver
    This change adds an experimental OpenContainer driver.
    
    Depends-On: I5e6d3969ae04f90abd1a3fd908c160cda4791bad
    Change-Id: I1b735bc20d193563d219cd79b935aeba0ad5441b

--- nodepool/provider_manager.py
+++ nodepool/provider_manager.py
@@ -19,6 +19,7 @@
 import logging
 
 from nodepool.driver.fake.provider import FakeProvider
+from nodepool.driver.oci.provider import OpenContainerProvider
 from nodepool.driver.openstack.provider import OpenStackProvider
 from nodepool.driver.static.provider import StaticNodeProvider
 
@@ -30,6 +31,8 @@ def get_provider_manager(provider, use_taskmanager):
         return OpenStackProvider(provider, use_taskmanager)
     elif provider.driver.name == 'static':
         return StaticNodeProvider(provider)
+    elif provider.driver.name == 'oci':
+        return OpenContainerProvider(provider)
     else:
         raise RuntimeError("Unknown provider driver %s" % provider.driver)
 
