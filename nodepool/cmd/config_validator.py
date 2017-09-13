208444d345326a1d966b2bd886a4ca3175a465a8
Tobias Henkel <tobias.henkel@bmw.de>
2017-09-13 23:37:51 +0200

    Honor cloud quotas before launching nodes
    This is an approach of calculating honoring the cloud quotas before
    launching nodes. For this the limits of the cloud are gathered and all
    unknown resources subtracted from the max values. Thus the remaining
    quota for being used by nodepool can be calculated and checked before
    launching nodes.
    
    Change-Id: I18594ab76949faddade3164b6027dd3b82771c95

--- nodepool/cmd/config_validator.py
+++ nodepool/cmd/config_validator.py
@@ -77,6 +77,7 @@ class ConfigValidator:
             'region-name': str,
             v.Required('cloud'): str,
             'max-concurrency': int,
+            'max-quota-age': int,
             'boot-timeout': int,
             'launch-timeout': int,
             'launch-retries': int,
