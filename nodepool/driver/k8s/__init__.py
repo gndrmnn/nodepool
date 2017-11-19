04540809a8aab83d06bb4320b0a256bbb20c20f1
Tristan Cacqueray <tdecacqu@redhat.com>
2017-11-19 03:59:28 +0000

    Implement a Kubernetes driver
    This change adds an experimental Kubernetes driver.
    
    TODO:
    * Add tests
    * Wait for zuul_stream refactor to fix console-stream
    
    Change-Id: I81b5dc5abe92b71cc98b0d71c8a2863cddff6027

---
+++ nodepool/driver/k8s/__init__.py
@@ -0,0 +1,25 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from nodepool.driver import Driver
+from nodepool.driver.k8s import config
+from nodepool.driver.k8s import provider
+
+
+class KubernetesDriver(Driver):
+    def getProviderConfig(self, provider):
+        return config.KubernetesProviderConfig(provider)
+
+    def getProvider(self, provider_config, use_taskmanager):
+        return provider.KubernetesProvider(provider_config, use_taskmanager)
