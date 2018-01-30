f92e8d18a38f5961afb59ae29425f7ccb74c2f98
David Shrewsbury <shrewsbury.dave@gmail.com>
2018-01-30 15:38:57 -0500

    Provider wedge fix
    If a provider is going to pause due to insufficient quota, we attempt
    to delete the oldest unused node (one that is READY and unlocked) to
    guarantee that some quota will be freed.
    
    NOTE: This causes some assumptions in tests depending on the current
    pause behavior (no proactive deletion) to no longer be valid, so steps
    are taken in those tests to prevent the proactive delete from happening.
    
    Change-Id: I8d94c60fe1ab184503592a02d6ca458f94a2ea3d

--- nodepool/zk.py
+++ nodepool/zk.py
@@ -1646,6 +1646,62 @@ class ZooKeeper(object):
                 ret[node.type].append(node)
         return ret
 
+    def deleteOldestUnusedNode(self, provider_name, pool_name):
+        '''
+        Deletes the oldest unused (READY+unlocked) node for a provider's pool.
+
+        :param str provider_name: Name of the provider.
+        :param str pool_name: Pool name for the given provider.
+
+        :returns: True if a delete was requested, False otherwise.
+        '''
+        def age(timestamp):
+            now = time.time()
+            dt = now - timestamp
+            m, s = divmod(dt, 60)
+            h, m = divmod(m, 60)
+            d, h = divmod(h, 24)
+            return '%02d:%02d:%02d:%02d' % (d, h, m, s)
+
+        candidates = []
+        for node in self.nodeIterator():
+            if (node.state == READY and
+                node.provider == provider_name and
+                node.pool == pool_name
+            ):
+                candidates.append(node)
+
+        candidates.sort(key=lambda n: n.state_time)
+        for node in candidates:
+            try:
+                self.lockNode(node, blocking=False)
+            except npe.ZKLockException:
+                continue
+
+            # Make sure the state didn't change on us
+            n = self.getNode(node.id)
+            if n.state != READY:
+                self.unlockNode(node)
+                continue
+
+            node.state = DELETING
+            try:
+                self.log.debug("Deleting unused node %s (age: %s)",
+                               node.id, age(node.state_time))
+                self.storeNode(node)
+            except Exception:
+                self.log.exception(
+                    "Error attempting to update unused node %s:", node.id)
+                continue
+            finally:
+                self.unlockNode(node)
+
+            # If we got here, we found and requested a delete for the
+            # oldest unused node.
+            return True
+
+        return False
+
     def nodeIterator(self):
         '''
         Utility generator method for iterating through all nodes.
