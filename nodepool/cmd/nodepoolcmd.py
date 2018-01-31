1270c5656df81769275b5b6dd82a977b75235f6e
mhuin <mhuin@redhat.com>
2018-01-31 11:57:07 +0100

    Allow operator to set held node's TTL
    Add an option called --hold-for to `nodepool hold`. This option
    allows an operator to specify how long a node should be in HOLD
    status before being deleted (in seconds). The option overrides
    the default "max-hold-age" value in the configuration if set.
    
    Change-Id: Ide93fb13d2ee433cdf60877102a4389fe2698d28

--- nodepool/cmd/nodepoolcmd.py
+++ nodepool/cmd/nodepoolcmd.py
@@ -82,6 +82,11 @@ class NodePoolCmd(NodepoolApp):
         cmd_hold.add_argument('--reason',
                               help='Reason this node is held',
                               required=True)
+        cmd_hold.add_argument('--hold-for',
+                              help=('how long to hold the node for (seconds), '
+                                    'defaults to nodepool\'s "max-hold-age" '
+                                    'if set, indefinitely otherwise'),
+                              required=False, default=0)
 
         cmd_delete = subparsers.add_parser(
             'delete',
@@ -220,6 +225,8 @@ class NodePoolCmd(NodepoolApp):
 
         node.state = zk.HOLD
         node.comment = self.args.reason
+        if int(self.args.hold_for) > 0:
+            node.hold_for = int(self.args.hold_for)
         print("Waiting for lock...")
         self.zk.lockNode(node, blocking=True)
         self.zk.storeNode(node)
