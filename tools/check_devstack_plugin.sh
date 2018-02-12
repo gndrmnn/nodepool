ace8923a2c45705c73d5b22ee33ead01e7aa5b69
Ian Wienand <iwienand@redhat.com>
2018-02-12 11:28:54 +1100

    Add native distro test jobs
    This implements legacy-dsvm-nodepool-[distro]-src jobs as native jobs.
    This seems like the best home for them, as they are run by multiple
    externals such as glean and diskimage-builder.
    
    Previously the defaults were set such that with no arguments, the
    trusty test ran for nodepool-functional-py35.
    
    To facilitate better templating, this turns all builds off by default.
    The nodepool functional test is moved to a base template, and then
    nodepool-functional-py35 explicitly builds and boots Xenial now
    (trusty doesn't seem very useful).
    
    The check_devstack_plugin.sh script runs after devstack, so needs to
    source the stackrc file to pull in the variables about what images are
    paused or not.
    
    Additionally, the path for the script is fixed to nodepool so we can
    run the job from other projects.
    
    The redhat, ubuntu and suse legacy tests are re-implemented for their
    respective builds.
    
    This also highlighted that the opensuse test wasn't actually doing
    anything.  This actually adds the configuration to build opensuse-423.
    
    Needed-By: https://review.openstack.org/543270
    Needed-By: https://review.openstack.org/543328
    Needed-By: https://review.openstack.org/543329
    Needed-By: https://review.openstack.org/543330
    Change-Id: I203d149a1d63ac8358e1c8b878d9c2bc0ba67c02

--- tools/check_devstack_plugin.sh
+++ tools/check_devstack_plugin.sh
@@ -13,15 +13,22 @@ NODEPOOL_CONFIG=${NODEPOOL_CONFIG:-/etc/nodepool/nodepool.yaml}
 NODEPOOL_SECURE=${NODEPOOL_SECURE:-/etc/nodepool/secure.conf}
 NODEPOOL="$NODEPOOL_INSTALL/bin/nodepool -c $NODEPOOL_CONFIG -s $NODEPOOL_SECURE"
 
-# Flags to control which images we build.
-# NOTE(pabelanger): Be sure to also update devstack/settings if you change the
-# defaults.
-NODEPOOL_PAUSE_CENTOS_7_DIB=${NODEPOOL_PAUSE_CENTOS_7_DIB:-true}
-NODEPOOL_PAUSE_DEBIAN_JESSIE_DIB=${NODEPOOL_PAUSE_DEBIAN_JESSIE_DIB:-true}
-NODEPOOL_PAUSE_FEDORA_25_DIB=${NODEPOOL_PAUSE_FEDORA_25_DIB:-true}
-NODEPOOL_PAUSE_FEDORA_26_DIB=${NODEPOOL_PAUSE_FEDORA_26_DIB:-true}
-NODEPOOL_PAUSE_UBUNTU_TRUSTY_DIB=${NODEPOOL_PAUSE_UBUNTU_TRUSTY_DIB:-false}
-NODEPOOL_PAUSE_UBUNTU_XENIAL_DIB=${NODEPOOL_PAUSE_UBUNTU_XENIAL_DIB:-true}
+# Source stackrc so that we get the variables about enabled images set
+# from the devstack job.  That's the ones we'll wait for below.  Note
+# devstack's trueorfalse function has turned this into "True" and
+# "False" (with capitals...)
+if [[ ! -f /opt/stack/devstack/stackrc ]]; then
+    echo "Can not find enabled images from devstack run!"
+    exit 1
+else
+    source /opt/stack/devstack/stackrc
+fi
+NODEPOOL_PAUSE_CENTOS_7_DIB=${NODEPOOL_PAUSE_CENTOS_7_DIB:-True}
+NODEPOOL_PAUSE_DEBIAN_JESSIE_DIB=${NODEPOOL_PAUSE_DEBIAN_JESSIE_DIB:-True}
+NODEPOOL_PAUSE_FEDORA_25_DIB=${NODEPOOL_PAUSE_FEDORA_25_DIB:-True}
+NODEPOOL_PAUSE_FEDORA_26_DIB=${NODEPOOL_PAUSE_FEDORA_26_DIB:-True}
+NODEPOOL_PAUSE_UBUNTU_TRUSTY_DIB=${NODEPOOL_PAUSE_UBUNTU_TRUSTY_DIB:-True}
+NODEPOOL_PAUSE_UBUNTU_XENIAL_DIB=${NODEPOOL_PAUSE_UBUNTU_XENIAL_DIB:-True}
 
 function waitforimage {
     name=$1
@@ -45,42 +52,42 @@ function waitfornode {
     done
 }
 
-if [ $NODEPOOL_PAUSE_CENTOS_7_DIB = 'false' ]; then
+if [ $NODEPOOL_PAUSE_CENTOS_7_DIB = 'False' ]; then
     # check that image built
     waitforimage centos-7
     # check image was bootable
     waitfornode centos-7
 fi
 
-if [ $NODEPOOL_PAUSE_DEBIAN_JESSIE_DIB = 'false' ]; then
+if [ $NODEPOOL_PAUSE_DEBIAN_JESSIE_DIB = 'False' ]; then
     # check that image built
     waitforimage debian-jessie
     # check image was bootable
     waitfornode debian-jessie
 fi
 
-if [ $NODEPOOL_PAUSE_FEDORA_25_DIB = 'false' ]; then
+if [ $NODEPOOL_PAUSE_FEDORA_25_DIB = 'False' ]; then
     # check that image built
     waitforimage fedora-25
     # check image was bootable
     waitfornode fedora-25
 fi
 
-if [ $NODEPOOL_PAUSE_FEDORA_26_DIB = 'false' ]; then
+if [ $NODEPOOL_PAUSE_FEDORA_26_DIB = 'False' ]; then
     # check that image built
     waitforimage fedora-26
     # check image was bootable
     waitfornode fedora-26
 fi
 
-if [ $NODEPOOL_PAUSE_UBUNTU_TRUSTY_DIB = 'false' ]; then
+if [ $NODEPOOL_PAUSE_UBUNTU_TRUSTY_DIB = 'False' ]; then
     # check that image built
     waitforimage ubuntu-trusty
     # check image was bootable
     waitfornode ubuntu-trusty
 fi
 
-if [ $NODEPOOL_PAUSE_UBUNTU_XENIAL_DIB = 'false' ]; then
+if [ $NODEPOOL_PAUSE_UBUNTU_XENIAL_DIB = 'False' ]; then
     # check that image built
     waitforimage ubuntu-xenial
     # check image was bootable
