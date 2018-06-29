8d5ea2bae670ab4e7e2115f88268dd57bc5d801a
Tristan Cacqueray <tdecacqu@redhat.com>
2018-06-29 03:12:00 +0000

    launcher: add pool quota debug and log information
    This change logs the current pool quota as DEBUG and adds a
    INFO log when a pool doesn't have enough quota to satisfy a request.
    
    Change-Id: I78445f21f251d206d4fb0dce817a992685c8b319

--- nodepool/driver/__init__.py
+++ nodepool/driver/__init__.py
@@ -446,6 +446,9 @@ class NodeRequestHandler(NodeRequestHandlerNotifications,
                 # If we calculate that we're at capacity, pause until nodes
                 # are released by Zuul and removed by the DeletedNodeWorker.
                 if not self.hasRemainingQuota(ntype):
+                    self.log.info(
+                        "Not enough quota remaining to satisfy request %s",
+                        self.request)
                     if not self.paused:
                         self.log.debug(
                             "Pausing request handling to satisfy request %s",
