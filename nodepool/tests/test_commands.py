257681a1f1790da860ec26699ae0cf5ab10e2f61
Tristan Cacqueray <tdecacqu@redhat.com>
2018-06-29 03:05:36 +0000

    status: add pool column to nodepool list output
    This helps debug pool quota usage.
    
    Change-Id: Id6cdef978e1ba80a4fea5e5bfa862755fb6d7e40

--- nodepool/tests/test_commands.py
+++ nodepool/tests/test_commands.py
@@ -64,10 +64,10 @@ class TestNodepoolCMD(tests.DBTestCase):
     def assert_nodes_listed(self, configfile, node_cnt, status="ready",
                             detail=False, validate_col_count=False):
         cmd = ['list']
-        col_count = 9
+        col_count = 10
         if detail:
             cmd += ['--detail']
-            col_count = 17
+            col_count = 18
         if not validate_col_count:
             col_count = 0
         self.assert_listed(configfile, cmd, 6, status, node_cnt, col_count)
