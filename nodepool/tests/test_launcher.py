4c3277d718c6d359c7f27d2664d26cd49b00299e
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2018-07-10 12:30:44 +0100

    Add ability to ignore provider quota for a pool
    In some circumstances it is useful to tell the launcher to
    ignore the provide quota and just trust the max-* settings
    for that pool instead.
    
    This particular need arises when using Rackspace public cloud
    for both instances and OnMetal nodes. In this situation the
    quota for instances and for OnMetal nodes is different, but
    nodepool only queries the quota for instances. When trying to
    build OnMetal nodes, the quota check fails - but should not.
    
    In this circumstance, instead of making shade/nodepool
    complicated by figuring out how to calculate disparate quota
    types, it makes sense to rather just allow nodepool to ignore
    the quota for a pool to try executing the build instead.
    
    While this is our use-case, it may also be useful to others
    for other reasons.
    
    Change-Id: I232a1ab365795381ab180aceb48e8c87843ac713

--- nodepool/tests/test_launcher.py
+++ nodepool/tests/test_launcher.py
@@ -1436,3 +1436,57 @@ class TestLauncher(tests.DBTestCase):
         while pool_worker[0].paused_handler:
             time.sleep(0.1)
         self.assertEqual(0, len(pool_worker[0].request_handlers))
+
+    def test_ignore_provider_quota(self):
+        '''
+        Test that a node request get fulfilled with ignore-provider-quota set
+        to true.
+        '''
+
+        # Set max-cores quota value to 0 to force "out of quota". Note that
+        # the fake provider checks the number of instances during server
+        # creation to decide if it should throw an over quota exception,
+        # but it doesn't check cores.
+        def fake_get_quota():
+            return (0, 20, 1000000)
+        self.useFixture(fixtures.MockPatchObject(
+            fakeprovider.FakeProvider.fake_cloud, '_get_quota',
+            fake_get_quota
+        ))
+
+        configfile = self.setup_config('ignore_provider_quota_false.yaml')
+        self.useBuilder(configfile)
+        self.waitForImage('fake-provider', 'fake-image')
+
+        pool = self.useNodepool(configfile, watermark_sleep=1)
+        pool.start()
+
+        # Create a request with ignore-provider-quota set to false that should
+        # fail because it will decline the request because "it would exceed
+        # quota".
+        self.log.debug("Submitting request with ignore-provider-quota False")
+        req = zk.NodeRequest()
+        req.state = zk.REQUESTED
+        req.node_types.append('fake-label')
+        self.zk.storeNodeRequest(req)
+        req = self.waitForNodeRequest(req)
+        self.assertEqual(req.state, zk.FAILED)
+
+        configfile = self.setup_config('ignore_provider_quota_true.yaml')
+        self.useBuilder(configfile)
+        self.waitForImage('fake-provider', 'fake-image')
+
+        pool = self.useNodepool(configfile, watermark_sleep=1)
+        pool.start()
+
+        # Create a request with ignore-provider-quota set to true that should
+        # pass regardless of the lack of cloud/provider quota.
+        self.replace_config(configfile, 'ignore_provider_quota_true.yaml')
+
+        self.log.debug("Submitting request with ignore-provider-quota True")
+        req = zk.NodeRequest()
+        req.state = zk.REQUESTED
+        req.node_types.append('fake-label')
+        self.zk.storeNodeRequest(req)
+        req = self.waitForNodeRequest(req)
+        self.assertEqual(req.state, zk.FULFILLED)
