9b7ddfba7c6ce527b489169b19a0c85832881af2
Markus Hosch <Markus.Hosch@bmw.de>
2018-08-09 11:37:12 +0200

    Add list of metrics provided to statsd
    This change provides a list of currently available metrics that are
    reported by the laucher and the builder.
    
    Change-Id: I51bc38c746cab5374095cc80e77db4534c041119

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,6 +29,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinxcontrib.programoutput',
     'sphinxcontrib.httpdomain',
+    'zuul_sphinx',
+    'zuul.sphinx.zuul',
     'reno.sphinxext']
 #extensions = ['sphinx.ext.intersphinx']
 #intersphinx_mapping = {'python': ('http://docs.python.org/2.7', None)}
