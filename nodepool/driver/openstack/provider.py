10c1e2fa12c164df4b02430724b55ca1a92ad75a
Monty Taylor <mordred@inaugust.com>
2018-10-20 09:12:34 -0500

    Stop explicitly starting task manager thread
    sdk is running start on it directly now so this is causing an error.
    
    Change-Id: I893a6c03aa7cf9fa46a2a94daf3db3d4a7c9ddee

--- nodepool/driver/openstack/provider.py
+++ nodepool/driver/openstack/provider.py
@@ -55,7 +55,6 @@ class OpenStackProvider(Provider):
         if self._use_taskmanager:
             self._taskmanager = TaskManager(self.provider.name,
                                             self.provider.rate)
-            self._taskmanager.start()
         self.resetClient()
         self._zk = zk_conn
 
