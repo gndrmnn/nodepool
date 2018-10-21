48b028fd6f2d0ecc435ae791183472498d0e224e
Monty Taylor <mordred@inaugust.com>
2018-10-21 08:48:21 -0500

    Consume rate limiting task manager from openstacksdk
    openstacksdk now runs a rate limiting task manager by default. Use it by
    passing in rate information to the Connection.
    
    Depends-On: https://review.openstack.org/604521
    Change-Id: I8d17517a20b8d4bcb03bb147bd690df60ffa78a3

--- nodepool/driver/openstack/provider.py
+++ nodepool/driver/openstack/provider.py
@@ -25,7 +25,6 @@ from nodepool import exceptions
 from nodepool.driver import Provider
 from nodepool.driver.utils import QuotaInformation
 from nodepool.nodeutils import iterate_timeout
-from nodepool.task_manager import TaskManager
 from nodepool import version
 from nodepool import zk
 
@@ -46,26 +45,18 @@ class OpenStackProvider(Provider):
         self._networks = {}
         self.__flavors = {}  # TODO(gtema): caching
         self.__azs = None
-        self._use_taskmanager = use_taskmanager
-        self._taskmanager = None
         self._current_nodepool_quota = None
         self._zk = None
 
     def start(self, zk_conn):
-        if self._use_taskmanager:
-            self._taskmanager = TaskManager(self.provider.name,
-                                            self.provider.rate)
-            self._taskmanager.start()
         self.resetClient()
         self._zk = zk_conn
 
     def stop(self):
-        if self._taskmanager:
-            self._taskmanager.stop()
+        pass
 
     def join(self):
-        if self._taskmanager:
-            self._taskmanager.join()
+        pass
 
     def getRequestHandler(self, poolworker, request):
         return handler.OpenStackNodeRequestHandler(poolworker, request)
@@ -78,13 +69,9 @@ class OpenStackProvider(Provider):
         return self.__flavors
 
     def _getClient(self):
-        if self._use_taskmanager:
-            manager = self._taskmanager
-        else:
-            manager = None
         return openstack.connection.Connection(
             config=self.provider.cloud_config,
-            task_manager=manager,
+            rate_limit=self.provider.rate,
             app_name='nodepool',
             app_version=version.version_info.version_string()
         )
