fcd7252796910159704ef7804b7354df2cb81266
Tristan Cacqueray <tdecacqu@redhat.com>
2019-01-10 02:32:49 +0000

    Implement zookeeper-auth
    This change adds the zookeeper-auth to configure authentication and
    adds a nodepool-update-zk-auth script to manage ACL update.
    
    Change-Id: Ifc3ceda85d0103a0b06b0871ae95135c179ff02a

--- nodepool/tests/unit/test_commands.py
+++ nodepool/tests/unit/test_commands.py
@@ -38,7 +38,9 @@ class TestNodepoolCMD(tests.DBTestCase):
 
     def assert_listed(self, configfile, cmd, col, val, count, col_count=0):
         log = logging.getLogger("tests.PrettyTableMock")
-        self.patch_argv("-c", configfile, *cmd)
+        self.patch_argv("-c", configfile,
+                        "-s", self.setup_secure('zookeeper-auth.yaml'),
+                        *cmd)
         for _ in iterate_timeout(10, AssertionError, 'assert listed'):
             try:
                 with mock.patch('prettytable.PrettyTable.add_row') as \
@@ -85,7 +87,9 @@ class TestNodepoolCMD(tests.DBTestCase):
 
     def test_image_delete_invalid(self):
         configfile = self.setup_config("node_cmd.yaml")
-        self.patch_argv("-c", configfile, "image-delete",
+        self.patch_argv("-c", configfile,
+                        "-s", self.setup_secure('zookeeper-auth.yaml'),
+                        "image-delete",
                         "--provider", "invalid-provider",
                         "--image", "invalid-image",
                         "--build-id", "invalid-build-id",
@@ -97,7 +101,9 @@ class TestNodepoolCMD(tests.DBTestCase):
         self.useBuilder(configfile)
         self.waitForImage('fake-provider', 'fake-image')
         image = self.zk.getMostRecentImageUpload('fake-image', 'fake-provider')
-        self.patch_argv("-c", configfile, "image-delete",
+        self.patch_argv("-c", configfile,
+                        "-s", self.setup_secure('zookeeper-auth.yaml'),
+                        "image-delete",
                         "--provider", "fake-provider",
                         "--image", "fake-image",
                         "--build-id", image.build_id,
@@ -110,7 +116,10 @@ class TestNodepoolCMD(tests.DBTestCase):
         configfile = self.setup_config("node.yaml")
         self.useBuilder(configfile)
         self.waitForImage('fake-provider', 'fake-image')
-        self.patch_argv("-c", configfile, "alien-image-list")
+        self.patch_argv(
+            "-c", configfile,
+            "-s", self.setup_secure('zookeeper-auth.yaml'),
+            "alien-image-list")
         nodepoolcmd.main()
         self.assert_alien_images_empty(configfile)
 
@@ -122,7 +131,10 @@ class TestNodepoolCMD(tests.DBTestCase):
             fail_list))
 
         configfile = self.setup_config("node_cmd.yaml")
-        self.patch_argv("-c", configfile, "alien-image-list")
+        self.patch_argv(
+            "-c", configfile,
+            "-s", self.setup_secure('zookeeper-auth.yaml'),
+            "alien-image-list")
         nodepoolcmd.main()
 
     def test_list_nodes(self):
@@ -161,7 +173,10 @@ class TestNodepoolCMD(tests.DBTestCase):
     def test_config_validate(self):
         config = os.path.join(os.path.dirname(tests.__file__),
                               'fixtures', 'config_validate', 'good.yaml')
-        self.patch_argv('-c', config, 'config-validate')
+        self.patch_argv(
+            '-c', config,
+            "-s", self.setup_secure('zookeeper-auth.yaml'),
+            'config-validate')
         nodepoolcmd.main()
 
     def test_dib_image_list(self):
@@ -173,7 +188,10 @@ class TestNodepoolCMD(tests.DBTestCase):
     def test_dib_image_build_pause(self):
         configfile = self.setup_config('node_diskimage_pause.yaml')
         self.useBuilder(configfile)
-        self.patch_argv("-c", configfile, "image-build", "fake-image")
+        self.patch_argv(
+            "-c", configfile,
+            "-s", self.setup_secure('zookeeper-auth.yaml'),
+            "image-build", "fake-image")
         with testtools.ExpectedException(Exception):
             nodepoolcmd.main()
         self.assert_listed(configfile, ['dib-image-list'], 1, 'fake-image', 0)
@@ -214,8 +232,11 @@ class TestNodepoolCMD(tests.DBTestCase):
         self.assert_listed(configfile, ['dib-image-list'], 4, zk.READY, 1)
         builds = self.zk.getMostRecentBuilds(1, 'fake-image', zk.READY)
         # Delete the image
-        self.patch_argv('-c', configfile, 'dib-image-delete',
-                        'fake-image-%s' % (builds[0].id))
+        self.patch_argv(
+            '-c', configfile,
+            '-s', self.setup_secure('zookeeper-auth.yaml'),
+            'dib-image-delete',
+            'fake-image-%s' % (builds[0].id))
         nodepoolcmd.main()
         self.waitForBuildDeletion('fake-image', '0000000001')
         # Check that fake-image-0000000001 doesn't exist
@@ -236,7 +257,10 @@ class TestNodepoolCMD(tests.DBTestCase):
         self.assert_nodes_listed(configfile, 1, zk.READY)
 
         # Delete node
-        self.patch_argv('-c', configfile, 'delete', nodes[0].id)
+        self.patch_argv(
+            '-c', configfile,
+            '-s', self.setup_secure('zookeeper-auth.yaml'),
+            'delete', nodes[0].id)
         nodepoolcmd.main()
         self.waitForNodeDeletion(nodes[0])
 
@@ -268,7 +292,10 @@ class TestNodepoolCMD(tests.DBTestCase):
         self.assert_nodes_listed(configfile, 1, zk.READY)
 
         # Delete node
-        self.patch_argv('-c', configfile, 'delete', '--now', nodes[0].id)
+        self.patch_argv(
+            '-c', configfile,
+            '-s', self.setup_secure('zookeeper-auth.yaml'),
+            'delete', '--now', nodes[0].id)
         nodepoolcmd.main()
         self.waitForNodeDeletion(nodes[0])
 
@@ -285,7 +312,10 @@ class TestNodepoolCMD(tests.DBTestCase):
         image = self.zk.getMostRecentImageUpload('fake-image', 'fake-provider')
 
         # now do the manual build request
-        self.patch_argv("-c", configfile, "image-build", "fake-image")
+        self.patch_argv(
+            "-c", configfile,
+            "-s", self.setup_secure('zookeeper-auth.yaml'),
+            "image-build", "fake-image")
         nodepoolcmd.main()
 
         self.waitForImage('fake-provider', 'fake-image', [image])
@@ -310,7 +340,8 @@ class TestNodepoolCMD(tests.DBTestCase):
 
     def test_without_argument(self):
         configfile = self.setup_config("node_cmd.yaml")
-        self.patch_argv("-c", configfile)
+        self.patch_argv("-c", configfile,
+                        "-s", self.setup_secure('zookeeper-auth.yaml'))
         result = nodepoolcmd.main()
         self.assertEqual(1, result)
 
@@ -342,7 +373,9 @@ class TestNodepoolCMD(tests.DBTestCase):
 
         # Erase the data for the second provider
         self.patch_argv(
-            "-c", configfile, 'erase', 'fake-provider2', '--force')
+            "-c", configfile,
+            "-s", self.setup_secure('zookeeper-auth.yaml'),
+            'erase', 'fake-provider2', '--force')
         nodepoolcmd.main()
 
         # Verify that no build or node for the second provider is listed
