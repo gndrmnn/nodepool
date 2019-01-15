3b725086bbb50e903ceb204097758c11e8f39346
john.studarus <john@jhlconsulting.com>
2019-01-15 01:36:44 +0000

    pep8 corrections
    Change-Id: I0e1596b502047e9a4e278f6ba5f4bd863d64f5c5

--- nodepool/driver/packet/__init__.py
+++ nodepool/driver/packet/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2018 JHL Consulting LLC
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -25,6 +25,7 @@ class PacketDriver(Driver):
 
     def getProvider(self, provider_config, use_taskmanager):
         return PacketProvider(provider_config, use_taskmanager)
+
 
 class PacketProviderNotification(object):
     def nodeDeletedNotification(self, node):
