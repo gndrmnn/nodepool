9367cf8ed8c7c297974c04933d321e23b2e1eaa5
Ian Wienand <iwienand@redhat.com>
2019-07-23 13:26:06 +1000

    Add a dib-cmd option for diskimages
    This change allows you to specify a dib-cmd parameter for disk images,
    which overrides the default call to "disk-image-create".  This allows
    you to essentially decide the disk-image-create binary to be called
    for each disk image configured.
    
    It is inspired by a couple of things:
    
    The "--fake" argument to nodepool-builder has always been a bit of a
    wart; a case of testing-only functionality leaking across into the
    production code.  It would be clearer if the tests used exposed
    methods to configure themselves to use the fake builder.
    
    Because disk-image-create is called from the $PATH, it makes it more
    difficult to use nodepool from a virtualenv.  You can not just run
    "nodepool-builder"; you have to ". activate" the virtualenv before
    running the daemon so that the path is set to find the virtualenv
    disk-image-create.
    
    In addressing activation issues by automatically choosing the
    in-virtualenv binary in Ie0e24fa67b948a294aa46f8164b077c8670b4025, it
    was pointed out that others are already using wrappers in various ways
    where preferring the co-installed virtualenv version would break.
    
    With this, such users can ensure they call the "disk-image-create"
    binary they want.  We can then make a change to prefer the
    co-installed version without fear of breaking.
    
    In theory, there's no reason why a totally separate
    "/custom/venv/bin/disk-image-create" would not be valid if you
    required a customised dib for some reason for just one image.  This is
    not currently possible, even modulo PATH hacks, etc., all images will
    use the same binary to build.  It is for this flexibility I think this
    is best at the diskimage level, rather than as, say a global setting
    for the whole builder instance.
    
    Thus add a dib-cmd option for diskimages.  In the testing case, this
    points to the fake-image-create script, and the --fake command-line
    option and related bits are removed.
    
    It should have no backwards compatibility effects; documentation and a
    release note is added.
    
    Change-Id: I6677e11823df72f8c69973c83039a987b67eb2af

--- tools/zuul-nodepool-integration/start.sh
+++ tools/zuul-nodepool-integration/start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 cd "$(dirname "$0")"
 
@@ -7,5 +7,5 @@ mkdir -p /tmp/nodepool/log
 
 export OS_CLIENT_CONFIG_FILE=`pwd`/clouds.yaml
 
-nodepool-builder -c `pwd`/nodepool.yaml -l `pwd`/builder-logging.conf -p /tmp/nodepool/builder.pid --fake
+nodepool-builder -c `pwd`/nodepool.yaml -l `pwd`/builder-logging.conf -p /tmp/nodepool/builder.pid
 nodepool-launcher -c `pwd`/nodepool.yaml -s `pwd`/secure.conf -l `pwd`/launcher-logging.conf -p /tmp/nodepool/launcher.pid
