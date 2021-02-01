fd69ee999f68d5e37f57a954aa128385bb9a5210
Matthieu Huin <mhuin@redhat.com>
2021-02-01 17:18:53 +0100

    Move zuul integration testing to nodepool project
    Depends-On: https://review.opendev.org/760361
    Change-Id: Ia2cb8ab673841bf57e5319d04f207c26254e2805

---
+++ tools/zuul-nodepool-integration/zuul_pip_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Copyright 2020 Red Hat, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This is a barebone version of the tools/pip.sh that is found in zuul/zuul
+# and used to pre-install zuul and ansible before tests.
+
+set -e
+
+pip install $*
+
+# Install Ansible
+zuul-manage-ansible -v
