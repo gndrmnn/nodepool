1cb8f93fe520ed4c66b7d7a9b6ce18947c525a8d
James E. Blair <jim@acmegating.com>
2021-02-17 14:21:51 -0800

    WIP: Require TLS
    Require TLS Zookeeper connections before making the 4.0 release.
    
    Change-Id: I69acdcec0deddfdd191f094f13627ec1618142af
    Depends-On: https://review.opendev.org/776290

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -1,13 +1,46 @@
-#!/bin/bash -xe
+#!/bin/bash
 
 # This script will be run by OpenStack CI before unit tests are run,
 # it sets up the test system as needed.
 # Developers should setup their test systems in a similar way.
 
-# This setup needs to be run as a user that can run sudo.
+# This setup needs to be run as a user that can run docker or podman.
 
-# Config Zookeeper to run on tmpfs
-sudo service zookeeper stop
-DATADIR=$(sed -n -e 's/^dataDir=//p' /etc/zookeeper/conf/zoo.cfg)
-sudo mount -t tmpfs -o nodev,nosuid,size=500M none $DATADIR
-sudo service zookeeper start
+set -xeu
+
+cd $(dirname $0)
+SCRIPT_DIR="$(pwd)"
+
+# Select docker or podman
+if command -v docker > /dev/null; then
+  DOCKER="sudo docker"
+  if ! docker ps; then
+    sudo systemctl start docker
+  fi
+elif command -v podman > /dev/null; then
+  DOCKER=podman
+else
+  echo "Please install docker or podman."
+  exit 1
+fi
+
+# Select docker-compose or podman-compose
+if command -v docker-compose > /dev/null; then
+  COMPOSE="sudo docker-compose"
+elif command -v podman-compose > /dev/null; then
+  COMPOSE=podman-compose
+else
+  echo "Please install docker-compose or podman-compose."
+  exit 1
+fi
+
+CA_DIR=$SCRIPT_DIR/ca
+
+mkdir -p $CA_DIR
+$SCRIPT_DIR/zk-ca.sh $CA_DIR nodepool-test-zookeeper
+
+${COMPOSE} down
+
+${COMPOSE} up -d
+
+echo "Finished"
