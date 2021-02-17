e8c40cbc640cb18d296b35758990627cb3e46b0e
James E. Blair <jim@acmegating.com>
2021-02-17 14:21:51 -0800

    WIP: Require TLS
    Require TLS Zookeeper connections before making the 4.0 release.
    
    Change-Id: I69acdcec0deddfdd191f094f13627ec1618142af
    Depends-On: https://review.opendev.org/776290

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -1,13 +1,15 @@
-#!/bin/bash -xe
+#!/bin/bash
 
 # This script will be run by OpenStack CI before unit tests are run,
 # it sets up the test system as needed.
 # Developers should setup their test systems in a similar way.
 
-# This setup needs to be run as a user that can run sudo.
+# This setup needs to be run as a user that can run docker or podman.
 
-# Config Zookeeper to run on tmpfs
-sudo service zookeeper stop
-DATADIR=$(sed -n -e 's/^dataDir=//p' /etc/zookeeper/conf/zoo.cfg)
-sudo mount -t tmpfs -o nodev,nosuid,size=500M none $DATADIR
-sudo service zookeeper start
+set -xeu
+
+cd $(dirname $0)
+
+# The zuul user on OpenDev's test nodes is not part of the docker group,
+# so this script must be run as root.
+sudo ./test-setup-docker.sh
