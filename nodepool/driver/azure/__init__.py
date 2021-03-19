6d708b1ac7614690a2dbf8ad38c7e1f8739e7700
James E. Blair <jim@acmegating.com>
2021-03-19 14:15:09 -0700

    Azure: replace driver with state machine driver
    Now that the state-machine-based driver for Azure is complete,
    replace the current driver with it.  This changis is generally
    simply file renames, with some minor bugfixes to satisfy
    edge-cases that appear in the tests.
    
    This also updates the fake azure to accomodate the additional
    methods used by the new driver, and adds a test for image uploads
    and diskimage building.
    
    Change-Id: I6b5cf72501ea83a8a7a2f753ee6ed8d2e484a5d2

--- nodepool/driver/azure/__init__.py
+++ nodepool/driver/azure/__init__.py
@@ -1,27 +1,25 @@
-# Copyright 2018 Red Hat
+# Copyright 2021 Acme Gating, LLC
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-#
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
-from nodepool.driver import Driver
+from nodepool.driver.statemachine import StateMachineDriver
 from nodepool.driver.azure.config import AzureProviderConfig
-from nodepool.driver.azure.provider import AzureProvider
+from nodepool.driver.azure.adapter import AzureAdapter
 
 
-class AzureDriver(Driver):
+class AzureDriver(StateMachineDriver):
     def getProviderConfig(self, provider):
         return AzureProviderConfig(self, provider)
 
-    def getProvider(self, provider_config):
-        return AzureProvider(provider_config)
+    def getAdapter(self, provider_config):
+        return AzureAdapter(provider_config)
