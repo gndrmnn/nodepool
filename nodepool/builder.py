51914f50d1cdb8e1c1b313b66257ffa661261532
Benedikt Loeffler <benedikt.loeffler@bmw.de>
2021-07-14 12:27:52 +0200

    Add option for an upload script
    Add an option to use an alternative upload script.
    
    Change-Id: I148b853a23b1035e95def97d5b80fa12662b8251

--- nodepool/builder.py
+++ nodepool/builder.py
@@ -15,6 +15,7 @@
 import fcntl
 import logging
 import os
+import re
 import select
 import shutil
 import socket
@@ -1087,22 +1088,63 @@ class UploadWorker(BaseWorker):
         meta['nodepool_build_id'] = build_id
         meta['nodepool_upload_id'] = upload_id
 
-        try:
-            external_id = manager.uploadImage(
-                provider_image,
-                ext_image_name, filename,
-                image_type=image.extension,
-                meta=meta,
-                md5=image.md5,
-                sha256=image.sha256,
-            )
-        except Exception:
-            self.log.exception(
-                "Failed to upload build %s of image %s to provider %s" %
-                (build_id, image_name, provider.name))
-            data = zk.ImageUpload()
-            data.state = zk.FAILED
-            return data
+        if provider.upload_script:
+            try:
+                cmd = [
+                    provider.upload_script,
+                    ext_image_name,
+                    filename,
+                    provider.name,
+                    provider.provider["cloud"],
+                    build_id,
+                    upload_id,
+                ]
+                self.log.info('Running upload script %s', cmd)
+                p = subprocess.run(cmd, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE, check=True)
+                upload_script_output = p.stdout.decode()
+
+                # parse image id from script output
+                ids = re.findall(r"Image ID: ([\w-]+)", upload_script_output)
+                if not ids:
+                    raise Exception('Could not parse image id from upload '
+                                    'script output')
+                external_id = ids[0]
+
+            except Exception as e:
+                if isinstance(e, subprocess.CalledProcessError):
+                    self.log.error('Upload script failed with '
+                                   'exit code %s\nstdout:\n%s\nstderr:\n%s',
+                                   e.returncode, e.stdout.decode(),
+                                   e.stderr.decode())
+                else:
+                    self.log.exception(
+                        'Unknown exception during upload script')
+                data = zk.ImageUpload()
+                data.state = zk.FAILED
+                return data
+
+            self.log.info(
+                'Upload script success with exit code %s\n'
+                'stdout:\n%s\nstderr:\n%s',
+                p.returncode, p.stdout.decode(), p.stderr.decode())
+        else:
+            try:
+                external_id = manager.uploadImage(
+                    provider_image,
+                    ext_image_name, filename,
+                    image_type=image.extension,
+                    meta=meta,
+                    md5=image.md5,
+                    sha256=image.sha256,
+                )
+            except Exception:
+                self.log.exception(
+                    "Failed to upload build %s of image %s to provider %s" %
+                    (build_id, image_name, provider.name))
+                data = zk.ImageUpload()
+                data.state = zk.FAILED
+                return data
 
         if provider.post_upload_hook:
             try:
