7ea4814f533615a20b7304d1b3a11b368186779b
Ian Wienand <iwienand@redhat.com>
2022-01-27 13:01:03 +1100

    [wip] nodepool-zuul-functional: install into a venv
    Installing globally isn't working.  Let's not fight pip about this and
    convert the job to run out of a venv.
    
    Change-Id: Ic959ce1afa5a125e66586cb4c3c9c8ecd18fdd57

--- tools/zuul-nodepool-integration/start.sh
+++ tools/zuul-nodepool-integration/start.sh
@@ -7,5 +7,5 @@ mkdir -p /tmp/nodepool/log
 
 export OS_CLIENT_CONFIG_FILE=`pwd`/clouds.yaml
 
-nodepool-builder -c `pwd`/nodepool.yaml -l `pwd`/builder-logging.conf -p /tmp/nodepool/builder.pid
-nodepool-launcher -c `pwd`/nodepool.yaml -s `pwd`/secure.conf -l `pwd`/launcher-logging.conf -p /tmp/nodepool/launcher.pid
+/home/zuul/nodepool-venv/bin/nodepool-builder -c `pwd`/nodepool.yaml -l `pwd`/builder-logging.conf -p /tmp/nodepool/builder.pid
+/home/zuul/nodepool-venv/bin/nodepool-launcher -c `pwd`/nodepool.yaml -s `pwd`/secure.conf -l `pwd`/launcher-logging.conf -p /tmp/nodepool/launcher.pid
