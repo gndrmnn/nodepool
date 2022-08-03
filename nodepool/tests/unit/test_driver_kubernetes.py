bda63ad8ca22f2797b5c893bba76583785e30fbe
Benjamin Schanzel <benjamin.schanzel@bmw.de>
2022-08-03 14:41:23 +0200

    Implement cleanup of leaked resources in k8s driver
    As other drivers, cleanup resources (i.e. k8s namespaces) that are
    managed by nodepool (have nodepool specific metadata) but do not have
    a correpsonding zk node (anymore). We consider those leaked nodes that
    we want to clean up periodically.
    
    Change-Id: I6f8122861a42845a18e4d376e58ee4a48aa1df3a

--- nodepool/tests/unit/test_driver_kubernetes.py
+++ nodepool/tests/unit/test_driver_kubernetes.py
@@ -40,6 +40,7 @@ class FakeCoreClient(object):
         class FakeNamespace:
             class metadata:
                 name = ns_body['metadata']['name']
+                labels = ns_body['metadata']['labels']
         self.namespaces.append(FakeNamespace)
         return FakeNamespace
 
@@ -222,6 +223,32 @@ class TestDriverKubernetes(tests.DBTestCase):
     def test_kubernetes_tenant_quota_ram(self):
         self._test_kubernetes_quota(
             'kubernetes-tenant-quota-ram.yaml', pause=False)
+
+    def test_kubernetes_leaked_node(self):
+        conf = self.setup_config('kubernetes-leaked-node.yaml')
+        pool = self.useNodepool(conf, watermark_sleep=1)
+        pool.cleanup_interval = 1
+        pool.start()
+
+        # wait for min-ready node to be available
+        nodes = self.waitForNodes('pod-fedora')
+        self.assertEqual(len(nodes), 1)
+        manager = pool.getProviderManager('kubespray')
+        servers = manager.listNodes()
+        self.assertEqual(len(servers), 1)
+
+        # delete node from zk so it becomes 'leaked'
+        self.zk.deleteNode(nodes[0])
+
+        # node gets replaced, wait for that
+        new_nodes = self.waitForNodes('pod-fedora')
+        self.assertEqual(len(new_nodes), 1)
+
+        # original node should get deleted eventually
+        self.waitForInstanceDeletion(manager, nodes[0].external_id)
+
+        servers = manager.listNodes()
+        self.assertEqual(len(servers), 1)
 
     def _test_kubernetes_quota(self, config, pause=True):
         configfile = self.setup_config(config)
