1abd3a0d844a29ef92c190b774423f8a4bdf267d
mbecker <marvinbecker@mail.de>
2022-12-16 13:58:35 +0100

    Add additional pod specs to openshift driver
    These additional fields enable usage of gpu workloads with
    the openshift and openshiftpods drivers
    along with non-standard schedulers.
    
    Change-Id: Ieb85878708d67fd6361183f2b4373dc4d5f61afe

--- nodepool/driver/openshift/config.py
+++ nodepool/driver/openshift/config.py
@@ -48,10 +48,12 @@ class OpenshiftPool(ConfigPool):
             pl.image_pull_secrets = label.get('image-pull-secrets', [])
             pl.cpu = label.get('cpu')
             pl.memory = label.get('memory')
+            pl.gpu = label.get('gpu')
             pl.python_path = label.get('python-path', 'auto')
             pl.shell_type = label.get('shell-type')
             pl.env = label.get('env', [])
             pl.node_selector = label.get('node-selector')
+            pl.scheduler_name = label.get('scheduler-name')
             pl.pool = self
             self.labels[pl.name] = pl
             full_config.labels[label['name']].pools.append(self)
@@ -95,10 +97,12 @@ class OpenshiftProviderConfig(ProviderConfig):
             'image-pull-secrets': list,
             'cpu': int,
             'memory': int,
+            'gpu': dict,
             'python-path': str,
             'shell-type': str,
             'env': [env_var],
             'node-selector': dict,
+            'scheduler-name': str
         }
 
         pool = ConfigPool.getCommonSchemaDict()
