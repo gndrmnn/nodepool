ff315dd1f2ae6527ad9b375655ce1f3bf4fc4f54
Simon Westphahl <simon.westphahl@bmw.de>
2023-06-13 12:09:38 +0200

    Log cache startup errors and retry
    We've seen cases where the cache failed to start but no related
    exception could be found in the logs. This is now changed to also
    include the traceback
    
    In cases where the cache init fails, the Nodepool launcher will just
    hang there, waiting for the cache to be ready. To avoid such a situation
    we'll retry the cache init.
    
    Change-Id: I3bbc83ec8c3fbb382e3e39a84f6f5bad7dc2ee41

--- nodepool/zk/zookeeper.py
+++ nodepool/zk/zookeeper.py
@@ -812,7 +812,8 @@ class NodepoolTreeCache(abc.ABC):
                 self._ready.set()
                 self.log.debug("Cache at %s is ready", self.root)
             except Exception:
-                self.log.error("Error initializing cache at %s", self.root)
+                self.log.exception("Error initializing cache at %s", self.root)
+                self.zk.kazoo_client.handler.short_spawn(self._start)
             finally:
                 self._init_lock.release()
         else:
