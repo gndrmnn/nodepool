31737b1037943e45e3872c4d5ff9a7f9457564b6
Benjamin Schanzel <benjamin.schanzel@bmw.de>
2023-07-06 17:38:13 +0200

    Metastatic: fix user_data type
    User data is stored as a serialized JSON string by the metastatic driver
    but is expected to be returned as dict by kazoo at various places in
    zuul.
    To fix that, let kazoo do the de-/serialization, just as we do at other
    places.
    
    Change-Id: I5162c08ad2cc69154e4daa0f2700be50c28a7049

--- nodepool/tests/unit/test_driver_metastatic.py
+++ nodepool/tests/unit/test_driver_metastatic.py
@@ -194,9 +194,7 @@ class TestDriverMetastatic(tests.DBTestCase):
         self.assertEqual(nodes, [node1, bn1])
 
         # Update the node to indicate it was for a non-existent label
-        user_data = json.loads(bn1.user_data)
-        user_data['label'] = 'old-label'
-        bn1.user_data = json.dumps(user_data)
+        bn1.user_data['label'] = 'old-label'
         self.zk.storeNode(bn1)
 
         # Restart the provider and make sure we load data correctly
