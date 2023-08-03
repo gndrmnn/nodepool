2da0247afbe476f14045253eeb0385d05b12f4dd
Benedikt Loeffler <benedikt.loeffler@bmw.de>
2023-08-03 11:29:52 +0200

    Test if username is set for diskimage based nodes in AWS
    Change-Id: I4c13e95baf213831d02c31df0e91c82c2c86d3d8

--- nodepool/tests/unit/test_driver_aws.py
+++ nodepool/tests/unit/test_driver_aws.py
@@ -683,6 +683,7 @@ class TestDriverAws(tests.DBTestCase):
         self.assertIsNotNone(node.launcher)
         self.assertEqual(node.connection_type, 'ssh')
         self.assertEqual(node.shell_type, None)
+        self.assertEqual(node.username, 'another_user')
         self.assertEqual(node.attributes,
                          {'key1': 'value1', 'key2': 'value2'})
         self.assertEqual(
