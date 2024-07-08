85f366cba85799e902d4b8b3cdfbd4bbd4ccbd1e
Dong Zhang <dong.zhang@bmw.de>
2024-07-08 15:49:59 +0200

    WIP: Implement EC2 fleet API in AWS driver
    Change-Id: I0fe002590cf74add77da08b9366acc4babd44a46

--- nodepool/driver/aws/adapter.py
+++ nodepool/driver/aws/adapter.py
@@ -1486,6 +1486,129 @@ class AwsAdapter(statemachine.Adapter):
         else:
             image_id = self._getImageId(label.cloud_image)
 
+        if label.fleet:
+            return self._createFleet(label, image_id, tags, hostname, log)
+        else:
+            return self._runInstace(label, image_id, tags, hostname, log)
+
+    def _createFleet(self, label, image_id, tags, hostname, log):
+        log.debug('Creating instance with fleet')
+
+        overrides = []
+
+        instance_types = label.fleet.get('instance-types', [])
+        for instance_type in instance_types:
+            overrides.append({
+                'ImageId': image_id,
+                'InstanceType': instance_type,
+                'SubnetId': label.pool.subnet_id,
+            })
+
+        instance_requirements = label.fleet.get('instance-requirements')
+        if instance_requirements:
+            requirements = dict()
+            if instance_requirements.get('vcpu-count'):
+                requirements['VCpuCount'] = {}
+
+                if instance_requirements.get('vcpu-count').get('min'):
+                    requirements['VCpuCount']['Min'] = instance_requirements.get('vcpu-count').get('min')
+
+                if instance_requirements.get('vcpu-count').get('max'):
+                    requirements['VCpuCount']['Max'] = instance_requirements.get('vcpu-count').get('max')
+
+            if instance_requirements.get('memory-mib'):
+                requirements['MemoryMiB'] = {}
+
+                if instance_requirements.get('memory-mib').get('min'):
+                    requirements['MemoryMiB']['Min'] = instance_requirements.get('memory-mib').get('min')
+
+                if instance_requirements.get('memory-mib').get('max'):
+                    requirements['MemoryMiB']['Max'] = instance_requirements.get('memory-mib').get('max')
+
+            if instance_requirements.get('cpu-manufacturers'):
+                requirements['CpuManufacturers'] = instance_requirements.get('cpu-manufacturers')
+
+            if instance_requirements.get('excluded-instance-types'):
+                requirements['ExcludedInstanceTypes'] = instance_requirements.get('excluded-instance-types')
+
+            if instance_requirements.get('instance-generations'):
+                requirements['InstanceGenerations'] = instance_requirements.get('instance-generations')
+
+            overrides.append({
+                'ImageId': image_id,
+                'SubnetId': label.pool.subnet_id,
+                'InstanceRequirements': requirements,
+            })
+
+        if label.use_spot:
+            capacity_type_option = {
+                'SpotOptions': {
+                    'AllocationStrategy': label.fleet['allocation-strategy'],
+                },
+                'TargetCapacitySpecification': {
+                    'TotalTargetCapacity': 1,
+                    'DefaultTargetCapacityType': 'spot',
+                },
+            }
+        else:
+            capacity_type_option = {
+                'OnDemandOptions': {
+                    'AllocationStrategy': label.fleet['allocation-strategy'],
+                },
+                'TargetCapacitySpecification': {
+                    'TotalTargetCapacity': 1,
+                    'DefaultTargetCapacityType': 'on-demand',
+                },
+            }
+
+        # IO settings can not be overriten for now, templates are created by IO
+        # settings combinations, and the templates names are fixed.
+        launch_template_prefix = 'nodepool-launch-template'
+        template_name = (f'{launch_template_prefix}-'
+                         f'{label.volume_type}-{label.volume_size}-'
+                         f'{label.iops}-{label.throughput}')
+        log.debug("Template name: %", template_name)
+
+        args = {
+            **capacity_type_option,
+            'LaunchTemplateConfigs': [
+                {
+                    'LaunchTemplateSpecification': {
+                        'LaunchTemplateName': template_name,
+                        'Version': '$Latest',
+                    },
+                    'Overrides': overrides,
+                },
+            ],
+            'Type': 'instant',
+            'TagSpecifications': [
+                {
+                    'ResourceType': 'instance',
+                    'Tags': tag_dict_to_list(tags),
+                },
+                {
+                    'ResourceType': 'volume',
+                    'Tags': tag_dict_to_list(tags),
+                },
+            ],
+        }
+
+        with self.rate_limiter(log.debug, "Created fleet"):
+            log.debug("Creating VM with fleet %s", hostname)
+            log.debug("EC2 create fleet: %s", args)
+            resp = self.ec2_client.create_fleet(**args)
+            log.debug("EC2 create fleet result: %s", resp)
+
+            instance_id = resp['Instances'][0]['InstanceIds'][0]
+
+            describe_instances_result = self.ec2_client.describe_instances(
+                InstanceIds=[instance_id]
+            )
+            log.debug("Created VM %s as instance %s", hostname, instance_id)
+
+            return describe_instances_result['Reservations'][0]['Instances'][0]
+
+    def _runInstace(self, label, image_id, tags, hostname, log):
         args = dict(
             ImageId=image_id,
             MinCount=1,
